d36f0eed4b4f80397a5d45a7a777d64eaf4fd74a
Joe LoPresti <joe@scientiamobile.com>
2019-07-31 10:56:36 -0400

    Configure with create-react-library

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'react-imageengine'
+import ImageEngine from 'react-imageengine'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ImageEngine src='/images/imageengine-homepage.png' width={'500'} htmlAttributes={{alt:'ImageEngine image'}}/>
       </div>
     )
   }
